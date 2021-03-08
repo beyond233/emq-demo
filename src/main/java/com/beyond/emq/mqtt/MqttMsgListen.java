d52dd4d381aa50c35c6fab490b0fc176ea5c7b7f
beyond233 <414460547@qq.com>
2021-03-08 23:28:57 +0800

    事件监听

--- src/main/java/com/beyond/emq/mqtt/MqttMsgListen.java
+++ src/main/java/com/beyond/emq/mqtt/MqttMsgListen.java
@@ -3,11 +3,14 @@ package com.beyond.emq.mqtt;
 import com.beyond.emq.config.MqttProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 /**
  * mqtt消息监听
@@ -27,12 +30,23 @@ public class MqttMsgListen {
 
     @EventListener({ContextRefreshedEvent.class})
     public void initListen() {
+        MqttClient mqttClient = null;
         try {
-            MqttClient mqttClient = new MqttClient(mqttProperty.getServerAddr(), mqttProperty.getClientId(), null);
+            mqttClient = new MqttClient(mqttProperty.getServerAddr(), mqttProperty.getClientId(), null);
         } catch (MqttException e) {
             e.printStackTrace();
         }
-        emqClient.connect(mqttProperty.getUsername(), mqttProperty.getPassword());
-        emqClient.subscribe("hello/#", MqttQos.AT_LEAST_ONCE);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(mqttProperty.getUsername());
+        options.setPassword(mqttProperty.getPassword().toCharArray());
+        try {
+            if (mqttClient != null) {
+                mqttClient.setCallback(new MqttMsgCallback());
+                mqttClient.connect(options);
+                mqttClient.subscribe("testtopic/#", MqttQos.AT_LEAST_ONCE.getValue());
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
